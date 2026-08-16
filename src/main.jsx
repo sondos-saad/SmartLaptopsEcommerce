1761695b6ff599fdce7052db2125891c800be586
sondos-saad <sondossaad812@gmail.com>
2026-08-16 22:01:39 +0300

    err

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
-import React from "react"
-import { createRoot } from 'react-dom/client'
+
+// import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
      <BrowserRouter basename='/'>
     <App />
   </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>,
 )
