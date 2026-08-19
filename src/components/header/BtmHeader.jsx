ab94cc99e88cd2124426dc63cd4cf81bed50bcd6
sondos-saad <sondossaad812@gmail.com>
2026-08-19 18:15:38 +0300

    create btm header

--- src/components/header/BtmHeader.jsx
+++ src/components/header/BtmHeader.jsx
@@ -1,9 +1,22 @@
-
+import { IoMdMenu ,IoIosArrowDown} from "react-icons/io";
 
 function BtmHeader() {
   return (
-    <div className="">
-    
+    <div className="btm_header w-[80%] m-auto pt-5">
+        <div className="container">
+            <nav className="nav">
+
+                <div className="category_nav">
+                    <div className="category_btn flex items-center gap-3">
+                        <IoMdMenu className="text-xl"/>
+                        <p className="font-bold">Browse Category</p>
+                        <IoIosArrowDown className="text-xl"/>
+                    </div>
+
+                    <div className="category_nav_list"></div>
+                </div>
+            </nav>
+        </div>
     </div>
   )
 }
