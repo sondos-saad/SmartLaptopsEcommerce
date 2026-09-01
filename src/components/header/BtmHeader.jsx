697637680ccbde40ceaaa1c2c212c18d2b144c87
sondos-saad <sondossaad812@gmail.com>
2026-09-01 21:56:08 +0300

    nav

--- src/components/header/BtmHeader.jsx
+++ src/components/header/BtmHeader.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
 import { IoMdMenu ,IoIosArrowDown} from "react-icons/io";
+import { Link } from "react-router";
+
+
+const NavLinks= [
+    {title: "Home" , link: '/'},
+    {title: "About" , link: '/about'},
+    {title: "Accessories" , link: '/accessories'},
+    {title: "Blog" , link: '/blog'},
+    {title: "Contact" , link: '/contact'},
+]
 
 function BtmHeader() {
+    const [categories, setCategories] = useState([])
+
+    useEffect(()=>{
+        fetch('https://dummyjson.com/products/categories')
+        .then((res) =>res.json())
+        .then((data) => setCategories(data))
+    },[])
+
+    console.log(categories)
+
   return (
     <div className="btm_header w-[80%] m-auto pt-5">
         <div className="container">
@@ -13,7 +34,18 @@ function BtmHeader() {
                         <IoIosArrowDown className="text-xl"/>
                     </div>
 
-                    <div className="category_nav_list"></div>
+                    <div className="category_nav_list">
+                        {categories.map((category)=>(
+                             <Link to={category.slug}>{category.name}</Link>
+                        ))}
+                    </div>
+
+                    <div className="nav_links">
+                        {NavLinks.map((item)=>{
+                             <Link to={item.link}>{item.title}</Link>
+                        })}
+                       
+                    </div>
                 </div>
             </nav>
         </div>
