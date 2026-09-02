4d82fa138edcc0de844378ace241b4f736f23655
sondos-saad <sondossaad812@gmail.com>
2026-09-02 22:36:00 +0300

    btm header done and responsive

--- src/components/header/BtmHeader.jsx
+++ src/components/header/BtmHeader.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { IoMdMenu ,IoIosArrowDown} from "react-icons/io";
 import { Link } from "react-router";
+import { GoSignIn } from "react-icons/go";
+import { FaUserPlus } from "react-icons/fa";
+
 
 
 const NavLinks= [
@@ -23,31 +26,35 @@ function BtmHeader() {
     console.log(categories)
 
   return (
-    <div className="btm_header w-[80%] m-auto pt-5">
-        <div className="container">
-            <nav className="nav">
+    <div className="btm_header py-5 bg-blue-400 mt-4">
+        <div className="container w-[95%] md:w-[80%] m-auto flex justify-between items-center">
+            <nav className="nav w-[80%]">
 
-                <div className="category_nav">
-                    <div className="category_btn flex items-center gap-3">
-                        <IoMdMenu className="text-xl"/>
-                        <p className="font-bold">Browse Category</p>
-                        <IoIosArrowDown className="text-xl"/>
+                <div className="category_nav relative flex justify-between items-center">
+                    <div className="category_btn flex items-center md:gap-3 md:w-[35%]">
+                        <IoMdMenu className="text-xl "/>
+                        <p className="font-bold md:text-lg text-sm hidden lg:block">Browse Category</p>
+                        <IoIosArrowDown className="md:text-xl hidden lg:block"/>
                     </div>
 
-                    <div className="category_nav_list">
+                    <div className="category_nav_list flex flex-col gap-2 w-[220px] h-[500px] overflow-y-scroll absolute top-12 bg-gray-100 pt-2 hidden">
                         {categories.map((category)=>(
-                             <Link to={category.slug}>{category.name}</Link>
+                             <Link to={category.slug} className="border-b-1 border-gray-300 pb-2 pl-3 cursor-pointer">{category.name}</Link>
                         ))}
                     </div>
 
-                    <div className="nav_links">
-                        {NavLinks.map((item)=>{
-                             <Link to={item.link}>{item.title}</Link>
-                        })}
+                    <div className="nav_links flex gap-2 md:gap-10 items-center lg:w-[55%]">
+                        {NavLinks.map((item)=>(
+                            <Link to={item.link} className="hover:text-blue-800 font-bold md:text-lg text-sm">{item.title}</Link>
+                        ))}
                        
                     </div>
                 </div>
             </nav>
+            <div className="sign_regs_icon flex justify-between items-center gap-2">
+                <Link to="/"><GoSignIn className="md:text-xl"/></Link>
+                <Link to="/"><FaUserPlus className="md:text-xl"/></Link>
+            </div>
         </div>
     </div>
   )
